87d437af033570e633adcbb46ca85be42a3bb2cb
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-05-24 07:52:53 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import typing
 import uvicorn
 from fastapi import FastAPI
 import requests
@@ -7,18 +6,7 @@ import urllib.request
 app: FastAPI = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {'get succeeded'}
-
-
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: typing.Union[str, None] = None):
-    return dict(item_id=item_id, q=q)
-
-
-@app.post('C:/Users/nikolais/Documents/python-task/vehicles.csv')
-@app.get('/')
+@app.post('/')
 def write_csv():
     return {'post succeeded'}
 
@@ -26,8 +14,7 @@ def write_csv():
 if '__server__' == __name__:
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-
-req = urllib.request.Request('http://127.0.0.1:8000/')
+req = urllib.request.Request('http://127.0.0.1:8000')
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
 
