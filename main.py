41ba1b18b28357709af022ecd03a62187b791008
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-05-28 15:11:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 import requests
 
-login_url = 'https://api.baubuddy.de/index.php/v1/login'
-headers = {'principal': 'default_test', 'Authorization': 'Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz'}
-login_data = {'username': '365', 'password': '1'}
-login_response = requests.post(
-    login_url,
-    headers=headers,
-    json=login_data,
-)
+file_path = 'C:/Users/nikolais/Documents/python-task/vehicles.csv'
+target_url = 'http://127.0.0.1:8000/'
+target_file = open(file_path, 'rb')
 
-json_response = login_response.json()
-if login_response.status_code != 200 or 'oauth' not in json_response:
-    print(
-        f'Failure: {login_response.status_code} {login_response.text}',
-    )
-    exit(1)
-else:
-    token_info = json_response['oauth']
 
-repost = requests.get('http://127.0.0.1:8000/')
-
-url = 'http://127.0.0.1:8000/'
-files = {'file': ('vehicles.csv', 'rb')}
-
-r = requests.get(url, files=files)
-var = r.text
-
-headers: dict[str, str] = {'Authorization': 'Bearer', 'json_response': 'access_token'}
-print(files)
-
-file_data = open('vehicles.csv')
-
-r = requests.get(url, files={'vehicles.csv': file_data})
-
+r = requests.get(target_url, files={'vehicles.csv': target_file})
 print(r.status_code)
--- server.py
+++ server.py
@@ -1,31 +1,9 @@
-import uvicorn
-from fastapi import FastAPI
-import requests
-import urllib.request
+from fastapi import FastAPI, File, UploadFile
+from typing_extensions import Annotated
 
-app: FastAPI = FastAPI()
+app = FastAPI()
 
 
-@app.post('/')
-def write_csv():
-    return {'post succeeded'}
-
-
-if '__server__' == __name__:
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-req = urllib.request.Request('http://127.0.0.1:8000')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-
-url = 'https://api.baubuddy.de/index.php/v1/login'
-payload = {
-    'username': '365',
-    'password': '1'
-}
-headers = {
-    'Authorization': 'Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz',
-    'Content-Type': 'application/json'
-}
-response = requests.request('POST', url, json=payload, headers=headers)
-print(response.text)
+@app.post('/upload file/')
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
