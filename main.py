9d5a43662776139c1a820e3e90a1ea4e6b6f6578
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-05-29 09:26:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ file_path = 'C:/Users/nikolais/Documents/python-task/vehicles.csv'
 target_url = 'http://127.0.0.1:8000/'
 target_file = open(file_path, 'rb')
 
-r = requests.get(target_url, files={'vehicles.csv': target_file})
+r = requests.post(target_url, files={'vehicles.csv': target_file})
 print(r.status_code)
