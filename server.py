25ef81066732eccebae1a3327a4da5ddb2d82846
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-06-06 14:44:19 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,28 @@
+import logging
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 
 app = FastAPI()
+logger = logging.getLogger("api")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("api.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
 
 
 @app.post('/upload-csv/')
 async def root(file: UploadFile = File(...)):
+    logger.info("POST request received for /upload-csv/")
     return {"filename": file.filename}
 
 
