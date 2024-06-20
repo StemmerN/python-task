d7be2a72f7024b6d031ee19cfe21e81693c308f8
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-06-20 11:02:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
 import requests
 import os
 import argparse
-from flask import Flask, request, jsonify
+import logging
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler('client.log'),
+        logging.StreamHandler()
+    ]
+)
 
 target_url = 'http://127.0.0.1:8000/upload-csv/'
 fixed_token = 'TestToken'
 
 parser = argparse.ArgumentParser(description='Client-Skript zum Hochladen einer CSV-Datei.')
-parser.add_argument('-p', '--csv-path', type=str, help='Pfad zur CSV-Datei')
+parser.add_argument('-s', '--csv-path', type=str, required=True, help='Pfad zur CSV-Datei')
 args = parser.parse_args()
 
-if args.csv_path:
-    file_path = args.csv_path
-else:
-    file_path = 'C:/Users/nikolais/Documents/python-task/vehicles.csv'
+file_path = args.csv_path
 
 if os.path.exists(file_path):
-    print("Die Datei existiert.")
+    logging.info("Die Datei existiert.")
 else:
-    print("Die Datei existiert nicht.")
+    logging.error("Die Datei existiert nicht.")
     exit()
 
+logging.info("Starte Login-Prozess.")
 login_response = requests.post('http://127.0.0.1:8000/login', json={'username': 'Test', 'password': '123456'})
 if login_response.status_code == 200:
     login_data = login_response.json()
     if login_data['login']:
         token = fixed_token
+        logging.info("Login erfolgreich.")
 
+        logging.info("Starte Upload der Datei.")
         with open(file_path, 'rb') as file_data:
             files = {'file': file_data}
             headers = {'Authorization': token}
             response = requests.post(target_url, files=files, headers=headers)
-
-        print(response.status_code)
+            logging.info(f"Upload abgeschlossen. Statuscode: {response.status_code}")
     else:
-        print('Login fehlgeschlagen.')
+        logging.error('Login fehlgeschlagen.')
 else:
-    print('Fehler beim Login')
+    logging.error('Fehler beim Login')
+
+# python .\main.py -s C:\Users\nikolais\Documents\python-task\vehicles.csv
+
