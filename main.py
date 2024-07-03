25e775f4e97914e71a107849a31ea18653852bd4
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-07-03 09:19:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,13 @@
 import requests
-import os
-import argparse
-import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    handlers=[
-        logging.FileHandler('client.log'),
-        logging.StreamHandler()
-    ]
-)
-
+file_path = 'C:/Users/nikolais/Documents/python-task/vehicles.csv'
 target_url = 'http://127.0.0.1:8000/upload-csv/'
 
-parser = argparse.ArgumentParser(description='Skript zum Hochladen einer CSV-Datei.')
-parser.add_argument('-s', '--csv-path', type=str, required=True, help='Pfad zur CSV-Datei')
-args = parser.parse_args()
+with open(file_path, 'rb') as file_data:
+    files = {'file': file_data}
+    r = requests.post(target_url, files=files)
 
-file_path = args.csv_path
-
-if os.path.exists(file_path):
-    logging.info('Die Datei existiert.')
-else:
-    logging.error('Die Datei existiert nicht.')
-    exit()
-
-logging.info('Starte Login-Prozess.')
-login_response = requests.post('http://127.0.0.1:8000/login',
-                               json={'username': 'Test', 'password': '123456', 'token': 'TestToken'}, )
-if login_response.status_code == 200:
-    login_data = login_response.json()
-    if login_data['login']:
-        token = login_data['token']
-        logging.info('Login erfolgreich.')
-
-        logging.info('Starte Upload der Datei.')
-        with open(file_path, 'rb') as file_data:
-            files = {'file': file_data}
-            headers = {'Authorization': f'Bearer {token}'}
-            response = requests.post(target_url, files=files, headers=headers)
-            logging.info(f'Upload abgeschlossen. Statuscode: {response.status_code}')
-    else:
-        logging.error('Login fehlgeschlagen.')
-        print(login_response)
-
-else:
-    logging.error('Fehler beim Login')
-    print(login_response)
+print(r.status_code)
 
 # python .\main.py -s C:\Users\nikolais\Documents\python-task\vehicles.csv
 # python .\main.py -s C:\Users\nicolai\PycharmProjects\python-task\vehicles.csv
--- server.py
+++ server.py
@@ -1,56 +1,14 @@
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
-from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, Header
-from fastapi.security import HTTPBasicCredentials, HTTPBasic
-import logging
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    handlers=[
-        logging.FileHandler('server.log'),
-        logging.StreamHandler()
-    ]
-)
 
 app = FastAPI()
-security = HTTPBasic()
-
-users_db = {
-    'Test': {'username': 'Test', 'password': '123456'}}
-fixed_token = 'TestToken'
-
-
-def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    user = users_db.get(credentials.username)
-    if user is None or user['password'] != credentials.password:
-        logging.error('Ungültige Anmeldeinformationen')
-        raise HTTPException(status_code=401, detail='Ungültige Anmeldeinformationen')
-    logging.info(f'Benutzer {credentials.username} hat sich angemeldet.')
-    return user
-
-
-@app.post('/login')
-def login(user: dict = Depends(get_current_user)):
-    logging.info('Erfolgreiches Login')
-    return {'username': user['username'], 'token': fixed_token, 'login': True}
 
 
 @app.post('/upload-csv/')
-async def upload_csv(file: UploadFile = File(...), token: str = Header(None)):
-    if token is None or token != fixed_token:
-        logging.error('Ungültiges Token')
-        raise HTTPException(status_code=401, detail='Ungültiges Token')
-
-    logging.info(f'Uploading file: {file.filename}')
-    return {'filename': file.filename}
-
-
-@app.get('/')
-def read_root():
-    logging.info('Rootendpoint aufgerufen')
-    return {'message': 'Welcome to the FastAPI application'}
+async def root(file: UploadFile = File(...)):
+    return {"filename": file.filename}
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+    
