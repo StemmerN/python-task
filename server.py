ec1d129750c50371223f467a896b5af39a9baf11
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-07-12 09:08:15 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -19,8 +19,8 @@ async def upload_csv(file: UploadFile = File(...)):
 
         return JSONResponse(content={"message": "CSV-Datei erfolgreich gelesen und verarbeitet"}, status_code=200)
     except pd.errors.ParserError as e:
-        logging.exception("Fehler beim Parsen der CSV-Datei")
-        return JSONResponse(content={"message": "Fehler beim Parsen der CSV-Datei", "error": str(e)}, status_code=400)
+        logging.exception("Fehler beim lesen der CSV-Datei")
+        return JSONResponse(content={"message": "Fehler beim verarbeiten der CSV-Datei", "error": str(e)}, status_code=400)
     except Exception as e:
         logging.exception("Ein unerwarteter Fehler ist aufgetreten")
         return JSONResponse(content={"message": "Upload fehlgeschlagen", "error": str(e)}, status_code=500)
