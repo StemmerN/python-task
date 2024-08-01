781999866d7426519a6d184ab9e371f5ec7fdd66
StemmerN <148344952+StemmerN@users.noreply.github.com>
2024-08-01 10:16:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,43 +5,47 @@ import requests
 import sys
 
 # Ziel-URL für den Upload
-target_url = 'http://127.0.0.1:8000/upload-csv/'
+target_url = 'http://127.0.0.1:8001/upload-csv/'
 
 # Konfiguration des Loggings
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Argumentparser für Kommandozeilenargumente
-parser = argparse.ArgumentParser(description='Skript zum Hochladen einer CSV-Datei.')
-parser.add_argument('-p', '--csv-path', type=str, required=True, help='Pfad zur CSV-Datei')
-parser.add_argument('-v', '--verbose', action='store_true', help='Erhöhte Ausgabedetails')
-args = parser.parse_args()
 
-# Erhöhte Ausgabedetails aktivieren, wenn das entsprechende Argument gesetzt ist
-if args.verbose:
-    logging.getLogger().setLevel(logging.DEBUG)
+def upload_csv(file_path, verbose=False):
+    # Erhöhte Ausgabedetails aktivieren, wenn das entsprechende Argument gesetzt ist
+    if verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
 
-file_path = args.csv_path
+    # Prüfen, ob die Datei existiert
+    if not os.path.exists(file_path):
+        logging.error('Die Datei existiert nicht.')
+        sys.exit(2)  # Statuscode 2 für Datei nicht gefunden
 
-# Prüfen, ob die Datei existiert
-if not os.path.exists(file_path):
-    logging.error('Die Datei existiert nicht.')
-    sys.exit(2)  # Statuscode 2 für Datei nicht gefunden
+    try:
+        with open(file_path, 'rb') as file_data:
+            files = {'file': file_data}
+            logging.info('Beginne mit dem Hochladen der Datei...')
+            response = requests.post(target_url, files=files)
+            response.raise_for_status()  # HTTP-Fehler auslösen, falls Statuscode nicht 200
+            logging.info('Datei erfolgreich hochgeladen.')
+            logging.debug(f'Status Code: {response.status_code}')
+            logging.debug(f'Response: {response.text}')
+    except requests.exceptions.HTTPError as e:
+        logging.exception(f"HTTP-Fehler beim Hochladen der Datei: {e.response.text if e.response else 'Keine Antwort erhalten'}")
+        sys.exit(3)  # Statuscode 3 für Netzwerk- oder HTTP-Fehler
+    except Exception as e:
+        logging.exception('Ein unerwarteter Fehler ist aufgetreten')
+        sys.exit(1)  # Statuscode 1 für allgemeine Fehler
 
-try:
-    with open(file_path, 'rb') as file_data:
-        files = {'file': file_data}
-        logging.info('Beginne mit dem Hochladen der Datei...')
-        response = requests.post(target_url, files=files)
-        response.raise_for_status()  # HTTP-Fehler auslösen, falls Statuscode nicht 200
-        logging.info('Datei erfolgreich hochgeladen.')
-        logging.debug(f'Status Code: {response.status_code}')
-        logging.debug(f'Response: {response.text}')
-except requests.exceptions.HTTPError as e:
-    logging.exception(f"HTTP-Fehler beim Hochladen der Datei: {e.response.text if e.response else 'Keine Antwort erhalten'}")
-    sys.exit(3)  # Statuscode 3 für Netzwerk- oder HTTP-Fehler
-except Exception as e:
-    logging.exception('Ein unerwarteter Fehler ist aufgetreten')
-    sys.exit(1)  # Statuscode 1 für allgemeine Fehler
+
+if __name__ == '__main__':
+    # Argumentparser für Kommandozeilenargumente
+    parser = argparse.ArgumentParser(description='Skript zum Hochladen einer CSV-Datei.')
+    parser.add_argument('-p', '--csv-path', type=str, required=True, help='Pfad zur CSV-Datei')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Erhöhte Ausgabedetails')
+    args = parser.parse_args()
+
+    upload_csv(args.csv_path, args.verbose)
 
 
 # python .\main.py -p C:\Users\nikolais\Documents\python-task\vehicles.csv
--- server.py
+++ server.py
@@ -7,24 +7,28 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Willkommen bei der FastAPI Anwendung. Verfügbare Endpunkte: /upload-csv/"}
 
 
 @app.post('/upload-csv/')
 async def upload_csv(file: UploadFile = File(...)):
     try:
         contents = await file.read()
+        logging.debug(f"Dateiinhalt: {contents.decode('utf-8')}")
         df = pd.read_csv(io.StringIO(contents.decode('utf-8')))
         logging.info(f"CSV-Datei erfolgreich gelesen: {df.head()}")
-
         return JSONResponse(content={"message": "CSV-Datei erfolgreich gelesen und verarbeitet"}, status_code=200)
     except pd.errors.ParserError as e:
-        logging.exception("Fehler beim lesen der CSV-Datei")
-        return JSONResponse(content={"message": "Fehler beim verarbeiten der CSV-Datei", "error": str(e)}, status_code=400)
+        logging.exception("Fehler beim Lesen der CSV-Datei")
+        return JSONResponse(content={"message": "Fehler beim Verarbeiten der CSV-Datei", "error": str(e)}, status_code=400)
     except Exception as e:
         logging.exception("Ein unerwarteter Fehler ist aufgetreten")
         return JSONResponse(content={"message": "Upload fehlgeschlagen", "error": str(e)}, status_code=500)
 
-
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
